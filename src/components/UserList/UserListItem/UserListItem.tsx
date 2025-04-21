dc5c9e2da41f95efa7d892d503730b3dc6eca079
Funprod <131615685+Funprod@users.noreply.github.com>
2025-04-21 15:38:45 +0300

    Revert "feat(add): Component More Information"

--- src/components/UserList/UserListItem/UserListItem.tsx
+++ src/components/UserList/UserListItem/UserListItem.tsx
@@ -3,8 +3,7 @@ import * as Popover from '@radix-ui/react-popover';
 import dotsIcon from '@/assets/dotsIcon.svg';
 import deleteIcon from '@/assets/deleteUser.svg';
 import banIcon from '@/assets/banUser.svg';
-import type { User } from '@/generated/graphql.ts';
-import { useNavigate } from 'react-router-dom';
+import type {User} from '@/generated/graphql.ts';
 
 export const formatDate = (dateString: string) => {
     const date = new Date(dateString);
@@ -12,51 +11,45 @@ export const formatDate = (dateString: string) => {
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
 
-  return `${day}.${month}.${year}`;
+    return `${day}.${month}.${year}`;
 };
 
 type Props = {
-  user: User;
-  onDelete: (userId: number) => void;
-};
+    user: User
+    onDelete: (userId: number) => void
+}
 
-const UserListItem = ({ user, onDelete }: Props) => {
-  const route = useNavigate();
+const UserListItem = ({user, onDelete}: Props) => {
+    return (
+        <div className={s.user} key={user.id}>
+            <div className={s.userId}>{user.id}</div>
+            <div className={s.userName}>{user.userName}</div>
+            <div className={s.profileLink}>{user.profile.userName}</div>
+            <div className={s.dateAdded}>{formatDate(user.profile.createdAt)}</div>
+            <div>
+                <Popover.Root>
+                    <Popover.Trigger asChild>
+                        <img src={dotsIcon} alt={'banIcon'} className={s.dotsIcon}/>
+                    </Popover.Trigger>
 
-  const handleMoreInformation = (id: number) => {
-    route(`/moreInformation/${id}`);
-  };
-
-  return (
-    <div className={s.user} key={user.id}>
-      <div className={s.userId}>{user.id}</div>
-      <div className={s.userName}>{user.userName}</div>
-      <div className={s.profileLink}>{user.profile.userName}</div>
-      <div className={s.dateAdded}>{formatDate(user.profile.createdAt)}</div>
-      <div>
-        <Popover.Root>
-          <Popover.Trigger asChild>
-            <img src={dotsIcon} alt={'banIcon'} className={s.dotsIcon} />
-          </Popover.Trigger>
-
-          <Popover.Content className={s.popoverContainer}>
-            <div className={s.popoverItem} onClick={() => onDelete(user.id)}>
-              <img src={deleteIcon} alt={'deleteIcon'} />
-              <span>Delete User</span>
+                    <Popover.Content className={s.popoverContainer}>
+                        <div className={s.popoverItem} onClick={()=>onDelete(user.id)}>
+                            <img src={deleteIcon} alt={'deleteIcon'}/>
+                            <span>Delete User</span>
+                        </div>
+                        <div className={s.popoverItem} onClick={() => {}}>
+                            <img src={banIcon} alt={'banIcon'}/>
+                            <span>Ban in the system</span>
+                        </div>
+                        <div className={s.popoverItem} onClick={() => {}}>
+                            <img src={dotsIcon} alt={'banIcon'}/>
+                            <span>More Information</span>
+                        </div>
+                    </Popover.Content>
+                </Popover.Root>
             </div>
-            <div className={s.popoverItem} onClick={() => {}}>
-              <img src={banIcon} alt={'banIcon'} />
-              <span>Ban in the system</span>
-            </div>
-            <div className={s.popoverItem} onClick={() => handleMoreInformation(user.id)}>
-              <img src={dotsIcon} alt={'banIcon'} />
-              <span>More Information</span>
-            </div>
-          </Popover.Content>
-        </Popover.Root>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default UserListItem;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,4 +1,3 @@
-
 import {Login} from '@/components/Login/Login';
 import {Routes, Route, Navigate} from 'react-router-dom';
 import MainLayout from '@/widgets/MainLayout/ui/MainLayout.tsx';
@@ -7,6 +6,7 @@ import {Statistics} from '@/features/Statistics/Statistics.tsx';
 import {PaymentsList} from '@/features/PaymentsList/PaymentsList.tsx';
 import {PostsList} from '@/features/PostsList/PostsList.tsx';
 import {RequireAuth} from '@/widgets/MainLayout/ui/RequireAuth.tsx';
+
 
 const AppRouter = () => {
     return (
@@ -18,7 +18,6 @@ const AppRouter = () => {
                 <Route path="statistics" element={<Statistics />} />
                 <Route path="payments-list" element={<PaymentsList />} />
                 <Route path="posts-list" element={<PostsList />} />
-                 <Route path="/moreInformation/:id" element={<MoreInformation />} />
             </Route>
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
