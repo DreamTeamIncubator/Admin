1c22d148a409b895f7e3f7c9510c1f676cd7d2be
Funprod <dron12033@gmail.com>
2025-04-21 16:17:56 +0300

    feat(add): Component More Information

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
+import './App.css';
+import { Header } from './components/Header/Header.tsx';
 
-import './App.css'
-import {Header} from './components/Header/Header.tsx';
 import AppRouter from './routes/Router.tsx';
 // import {UserList} from '@/components/UserList/UserList.tsx';
 
 function App() {
-
-
   return (
     <>
-        <Header/>
-        <AppRouter/>
+      <Header />
+      <AppRouter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
