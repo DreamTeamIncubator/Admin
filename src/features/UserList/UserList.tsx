74d94575698f5f245d3db2518820fdf22133452e
Raevskaya Natalie <ya.brokkoli@gmail.com>
2025-04-25 10:38:27 +0300

    feat(ban user): filter logic added

--- src/features/UserList/UserList.tsx
+++ src/features/UserList/UserList.tsx
@@ -1,156 +1,168 @@
-import { GET_USERS } from '@/apollo/graphQL.ts'
-import { NetworkStatus, useQuery } from '@apollo/client'
-import { ChangeEvent, useState } from 'react'
-import { Input } from '@/components/Input/Input.tsx'
-import { RadixSelect } from '@/components/Select/RadixSelect.tsx'
-import type { User } from '@/generated/graphql.ts'
-import { useDebounce } from '@/common/hooks/useDebounce.ts'
-import { LoadingBar } from '@/components/LoadingBar/LoadingBar.tsx'
-import { UserListItem } from '@/features/UserList/UserListItem/UserListItem.tsx'
+import {GET_USERS} from '@/apollo/graphQL.ts'
+import {NetworkStatus, useQuery} from '@apollo/client'
+import {ChangeEvent, useState} from 'react'
+import {Input} from '@/components/Input/Input.tsx'
+import {RadixSelect} from '@/components/Select/RadixSelect.tsx'
+import type {User} from '@/generated/graphql.ts'
+import {useDebounce} from '@/common/hooks/useDebounce.ts'
+import {LoadingBar} from '@/components/LoadingBar/LoadingBar.tsx'
+import {UserListItem} from '@/features/UserList/UserListItem/UserListItem.tsx'
 import s from './UserList.module.scss'
-import { Pagination } from '@/components/Pagination/Pagination.tsx'
-import { SortIcon } from '@/components/SortIcon/SortIcon'
+import {Pagination} from '@/components/Pagination/Pagination.tsx'
+import {SortIcon} from '@/components/SortIcon/SortIcon'
 
 const selectOptions = [
-  { value: 'Blocked', label: 'Blocked' },
-  { value: 'Not Blocked', label: 'Not Blocked' },
+    {value: 'ALL', label: 'All'},
+    {value: 'BLOCKED', label: 'Blocked'},
+    {value: 'UNBLOCKED', label: 'Not Blocked'},
 ]
 
 export const UserList = () => {
-  const [inputValue, setInputValue] = useState('')
-  const [selectValue, setSelectValue] = useState(selectOptions[1])
-  const [page, setPage] = useState(1)
-  const [perPage, setPerPage] = useState(8)
-  const perPageOptions = [8, 16, 32, 64, 128]
+    const [inputValue, setInputValue] = useState('')
+    const [selectValue, setSelectValue] = useState(selectOptions[0])
+    const [page, setPage] = useState(1)
+    const [perPage, setPerPage] = useState(8)
+    const perPageOptions = [8, 16, 32, 64, 128]
 
-  const { data, refetch, previousData, networkStatus, variables } = useQuery(GET_USERS, {
-    variables: {
-      searchTerm: inputValue,
-      pageSize: perPage,
-      pageNumber: page,
-      sortBy: 'createdAt',
-      sortDirection: 'desc',
-      statusFilter: 'ALL',
-    },
-    fetchPolicy: 'cache-and-network',
-    notifyOnNetworkStatusChange: true,
-  })
-
-  //debounce
-  const debouncedSearch = useDebounce((value: string) => {
-    refetch({
-      searchTerm: value,
-      pageNumber: 1,
-      pageSize: perPage,
+    const {data, refetch, previousData, networkStatus, variables} = useQuery(GET_USERS, {
+        variables: {
+            searchTerm: inputValue,
+            pageSize: perPage,
+            pageNumber: page,
+            sortBy: 'createdAt',
+            sortDirection: 'desc',
+            statusFilter: selectValue.value,
+        },
+        fetchPolicy: 'cache-and-network',
+        notifyOnNetworkStatusChange: true,
     })
-  }, 3000)
 
-  const usersData = data?.getUsers || previousData?.getUsers
-  const totalCount = usersData?.pagination?.totalCount || usersData?.totalCount || 0
-  const totalPages = Math.ceil(totalCount / perPage)
-  const users = usersData?.users || []
+    //debounce
+    const debouncedSearch = useDebounce((value: string) => {
+        refetch({
+            searchTerm: value,
+            pageNumber: 1,
+            pageSize: perPage,
+        })
+    }, 3000)
 
-  const onPageChangeHandler = (newPage: number) => {
-    setPage(newPage)
-    refetch({
-      pageNumber: newPage,
-      pageSize: perPage,
-    })
-  }
+    const usersData = data?.getUsers || previousData?.getUsers
+    const totalCount = usersData?.pagination?.totalCount || usersData?.totalCount || 0
+    const totalPages = Math.ceil(totalCount / perPage)
+    const users = usersData?.users || []
 
-  const onPerPageChangeHandler = (newPerPage: number) => {
-    setPerPage(newPerPage)
-    setPage(1)
-    refetch({
-      pageNumber: 1,
-      pageSize: newPerPage,
-    })
-  }
+    const onPageChangeHandler = (newPage: number) => {
+        setPage(newPage)
+        refetch({
+            pageNumber: newPage,
+            pageSize: perPage,
+        })
+    }
 
-  const onChangeInputHandler = (value: string) => {
-    setInputValue(value)
-    setPage(1)
-    debouncedSearch(value)
-  }
+    const onPerPageChangeHandler = (newPerPage: number) => {
+        setPerPage(newPerPage)
+        setPage(1)
+        refetch({
+            pageNumber: 1,
+            pageSize: newPerPage,
+        })
+    }
 
-  // const onChangeSelectHandler = (value: any) => {
-  //     setSelectValue(value)
-  //     // Добавить логику фильтрации
-  // }
+    const onChangeInputHandler = (value: string) => {
+        setInputValue(value)
+        setPage(1)
+        debouncedSearch(value)
+    }
 
-  const loading = networkStatus === NetworkStatus.loading && !data?.getUsers?.users
+    const onChangeSelectHandler = (value: string) => {
+        const selected = selectOptions.find(s => s.value === value)
 
-  const handleSortBy = (sortBy: string) => {
-    const newDirection =
-      variables?.sortBy === sortBy ? (variables.sortDirection === 'asc' ? 'desc' : 'asc') : 'asc'
+        if(selected) {
+            setSelectValue(selected)
+        } else {
+            setSelectValue(selectOptions[0])
+        }
+        setPage(1)
+        refetch({
+            ...variables,
+            pageNumber: 1,
+            statusFilter: selected?.value,
+        })
+    }
 
-    refetch({
-      ...variables,
-      sortBy,
-      sortDirection: newDirection,
-      pageNumber: 1,
-    })
-  }
+    const loading = networkStatus === NetworkStatus.loading && !data?.getUsers?.users
 
-  return (
-    <div className={s.userListWrapper}>
-      <div className={s.inputContainer}>
-        <Input
-          value={inputValue}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            onChangeInputHandler(e.currentTarget.value)
-          }
-          placeholder={'Search'}
-        />
-        <RadixSelect
-          className={s.selectFilter}
-          contentClassName={s.selectContent}
-          triggerClassName={s.selectTrigger}
-          options={selectOptions}
-          value={selectValue.value}
-          onValueChange={(value) => handleSortBy(value)}
-        />
-      </div>
-      <div className={s.headerContainer}>
-        <div>User ID </div>
-        <div>
-          Username{' '}
-          <SortIcon
-            active={variables?.sortBy === 'userName'}
-            direction={variables?.sortDirection}
-            onClick={() => handleSortBy('userName')}
-          />
+    const handleSortBy = (sortBy: string) => {
+        const newDirection =
+            variables?.sortBy === sortBy ? (variables.sortDirection === 'asc' ? 'desc' : 'asc') : 'asc'
+
+        refetch({
+            ...variables,
+            sortBy,
+            sortDirection: newDirection,
+            pageNumber: 1,
+        })
+    }
+
+    return (
+        <div className={s.userListWrapper}>
+            <div className={s.inputContainer}>
+                <Input
+                    value={inputValue}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        onChangeInputHandler(e.currentTarget.value)
+                    }
+                    placeholder={'Search'}
+                />
+                <RadixSelect
+                    className={s.selectFilter}
+                    contentClassName={s.selectContent}
+                    triggerClassName={s.selectTrigger}
+                    options={selectOptions}
+                    value={selectValue.value}
+                    onValueChange={onChangeSelectHandler}
+                />
+            </div>
+            <div className={s.headerContainer}>
+                <div>User ID</div>
+                <div>
+                    Username{' '}
+                    <SortIcon
+                        active={variables?.sortBy === 'userName'}
+                        direction={variables?.sortDirection}
+                        onClick={() => handleSortBy('userName')}
+                    />
+                </div>
+                <div>Profile link</div>
+                <div>
+                    {' '}
+                    Date added{' '}
+                    <SortIcon
+                        active={variables?.sortBy === 'createdAt'}
+                        direction={variables?.sortDirection}
+                        onClick={() => handleSortBy('createdAt')}
+                    />
+                </div>
+                <div>Actions</div>
+            </div>
+            <div className={s.userList}>
+                {loading && (
+                    <div className={s.overlay}>
+                        <LoadingBar/>
+                    </div>
+                )}
+                {users.map((user: User) => (
+                    <UserListItem key={user.id} user={user} refetch={refetch} isBanned={user.userBan}/>
+                ))}
+            </div>
+            <Pagination
+                count={totalPages || 1}
+                onChange={onPageChangeHandler}
+                page={page}
+                perPage={perPage}
+                perPageOptions={perPageOptions}
+                onPerPageChange={onPerPageChangeHandler}
+            />
         </div>
-        <div>Profile link</div>
-        <div>
-          {' '}
-          Date added{' '}
-          <SortIcon
-            active={variables?.sortBy === 'createdAt'}
-            direction={variables?.sortDirection}
-            onClick={() => handleSortBy('createdAt')}
-          />
-        </div>
-        <div>Actions</div>
-      </div>
-      <div className={s.userList}>
-        {loading && (
-          <div className={s.overlay}>
-            <LoadingBar />
-          </div>
-        )}
-        {users.map((user: User) => (
-          <UserListItem key={user.id} user={user} refetch={refetch} isBanned={user.userBan} />
-        ))}
-      </div>
-      <Pagination
-        count={totalPages || 1}
-        onChange={onPageChangeHandler}
-        page={page}
-        perPage={perPage}
-        perPageOptions={perPageOptions}
-        onPerPageChange={onPerPageChangeHandler}
-      />
-    </div>
-  )
+    )
 }
